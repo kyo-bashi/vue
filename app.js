74d994de23909e85494c456843151254483e511f
kyobashi <1998ryuto1023@gmail.com>
2021-07-24 22:14:41 +0900

    on:click

---
+++ app.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+	// data,functions
+	data (){
+		return {
+			title: 'The Final Empire',
+			author: 'Brandon Sanderson',
+			age: 45
+		}
+	},
+	methods:{
+		changeTitle(title){
+			// this.title = 'Words of Randiance'
+			this.title = title
+		}
+	}
+})
+
+app.mount('#app')
