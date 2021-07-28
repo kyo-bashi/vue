a488e06bff29317ba7e3dcb88fcadfd90664a282
kyobashi <1998ryuto1023@gmail.com>
2021-07-28 09:11:28 +0900

    v-for

--- app.js
+++ app.js
@@ -3,27 +3,17 @@ const app = Vue.createApp({
 	data (){
 		return {
 			showBooks: true,
-			title: 'The Final Empire',
-			author: 'Brandon Sanderson',
-			age: 45,
-			x:0,
-			y:0
+			books:[
+				{title:'name of the wind',author:'patrick rothfuss'},
+				{title:'name of the kings',author:'brandon sanderson'},
+				{title:'name of the empire',author:'brandon sanderson'},
+			]
 		}
 	},
 	methods:{
 		toggleshowBooks(){
 			this.showBooks = !this.showBooks
 		},
-		handleEvent(e,data){
-			console.log(e,e.type);
-			if (data) {
-				console.log(data);
-			}
-		},
-		handleMousemove(e){
-			this.x = e.offsetX;
-			this.y = e.offsetY;
-		}
 	}
 })
 
