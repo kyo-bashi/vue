3c0b7f431228c18190566d7444b658c28fb644e0
kyobashi <1998ryuto1023@gmail.com>
2021-07-28 20:52:37 +0900

    dynamic_class

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const app = Vue.createApp({
 			title: 'The Final Empire',
 			author: 'Brandon Sanderson',
 			age: 45,
-      			books:[
-				{title:'name of the wind',author:'patrick rothfuss'},
-				{title:'name of the kings',author:'brandon sanderson'},
-				{title:'name of the empire',author:'brandon sanderson'},
-			]
+			showBooks:true,
+			books:[
+				{title:'name of the wind',author:'patrick rothfuss',img:'asset/1.jpg',isFav:true},
+				{title:'name of the kings',author:'brandon sanderson',img:'asset/2.jpg',isFav:false},
+				{title:'name of the empire',author:'brandon sanderson',img:'asset/3.jpg',isFav:true},
+			],
+			url:'http://www.thenetninja.co.uk'
 		}
 	},
 	methods:{
